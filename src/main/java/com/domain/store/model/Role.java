894a36d9d5d20581dea04bffbb14da0b7cd9eeb8
ali-ammar-kazmi <aliammarkazmi16@gmail.com>
2024-11-11 21:43:43 +0530

    added: spring security dependency and designed user_role model relation

---
+++ src/main/java/com/domain/store/model/Role.java
@@ -0,0 +1,29 @@
+package com.domain.store.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users = new HashSet<>();
+
+    public Role(String name){
+        this.name=name;
+    }
+}
--- src/main/java/com/domain/store/model/User.java
+++ src/main/java/com/domain/store/model/User.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -29,4 +31,10 @@ public class User {
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private Cart cart;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade =
+            {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles = new HashSet<>();
 }
